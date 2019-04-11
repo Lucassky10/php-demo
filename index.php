c2df18b339e790bcb3149c150d4a8ac1e30b99ef
Lucassky10 <45350808+Lucassky10@users.noreply.github.com>
2019-04-11 18:42:20 +0200

    update

--- index.php
+++ index.php
@@ -1,5 +1,56 @@
-<?php
 
-echo "<center><h1>Code. Push. Shift.</h1></center>";
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <link href="https://fonts.googleapis.com/css?family=Overpass" rel="stylesheet">
+    <title>Product example · Bootstrap</title>
+    <style>
 
-?>
+    #page{
+        background-image: url('./os.svg');
+        background-size: cover;
+    }
+
+    #logo{
+        width:30%;
+        height:auto;
+        margin-top:5%;
+        margin-left:20%;
+    }
+    #php{
+        color:whitesmoke;
+        font-family:'Overpass';
+        text-align: center;
+        margin-top:10%;
+        font-size:70px;
+    }
+    </style>
+
+  
+
+    <!-- Bootstrap core CSS -->
+</head>
+<body id="page">
+
+<div id="logo">
+    <img src="./logo.svg">
+</div>
+
+<div id="php">
+    <?php
+
+    echo "Code.Push.Shift.<br> Exectuté sur une image php";
+
+    ?>
+</div>
+
+
+
+
+</body>
+</html>
